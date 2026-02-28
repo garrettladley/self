f447e1016370a1ce0db03bbb56c80cb4008911e7
Garrett Mitchell Ladley <garrett.ladley14@gmail.com>
2026-02-28 15:45:29 -0500

    feat: seopus

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://garrettladley.com",
   output: "static",
   adapter: vercel(),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
